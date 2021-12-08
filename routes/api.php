4ba837a4f0b9f5c8f7c44cc62ea7cc8d1a697131
Your Name <you@example.com>
2021-12-08 22:54:19 +0200

    first commit

--- routes/api.php
+++ routes/api.php
@@ -7,16 +7,7 @@ use App\Http\Controllers\Api\SoloController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+
 
 
 //messages
@@ -41,6 +32,12 @@ Route::post('login',[AuthController::class,'loginUser']);
 Route::group(['middleware' => ['jwt.verify']], function() {
     Route::get('user',[AuthController::class,'userData']);
     Route::post('update-user',[AuthController::class,'updateUser']);
+
+    //favorite
+
+    
+
+    //end favorite
 
     Route::get('logout', [AuthController::class, 'logout']);
 });
